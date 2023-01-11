2958b5fa43a5db0c70106383240a58f778dd4ee8
vniroshan <nnirosh447@gmail.com>
2023-01-11 18:50:03 +0530

    CUED added

--- client/server.js
+++ client/server.js
@@ -1,6 +1,7 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
 const client = require("./client");
+fastify.register(require("@fastify/formbody"));
 
 fastify.register(require("point-of-view"), {
   engine: {
@@ -31,12 +32,47 @@ fastify.get("/", async (request, reply) => {
 
   try {
     await client.getAll(null, (err, data) => {
+      console.log(data.todo);
       return data.todo;
     });
     return reply.view("./views/index.ejs", { todo: todo });
   } catch {
-    return res.status(400).send({ mgs: "error" });
+    return reply.status(400).send({ mgs: "error" });
   }
+});
+
+fastify.post("/add", async (request, reply) => {
+  let newTodo = {
+    title: request.body.title,
+    body: request.body.body,
+  };
+  client.insert(newTodo, (err, data) => {
+    if (err) throw err;
+    console.log("Todo created successfully", data);
+  });
+  return reply.redirect("/");
+});
+
+fastify.post("/update", async (request, reply) => {
+  const updateTodo = {
+    id: request.body.id,
+    title: request.body.title,
+    body: request.body.body,
+  };
+
+  client.update(updateTodo, (err, data) => {
+    if (err) throw err;
+    console.log("Todo updated successfully", data);
+  });
+  return reply.redirect("/");
+});
+
+fastify.post("/delete", async (request, reply) => {
+  client.remove({ id: request.body.id }, (err, _) => {
+    if (err) throw err;
+    console.log("Todo removed successfully");
+  });
+  return reply.redirect("/");
 });
 
 // Run the server!
--- server/server.js
+++ server/server.js
@@ -4,91 +4,88 @@ var grpc = require("grpc");
 var protoLoader = require("@grpc/proto-loader");
 
 var packageDefinition = protoLoader.loadSync(PROTO_PATH, {
-	keepCase: true,
-	longs: String,
-	enums: String,
-	arrays: true
+  keepCase: true,
+  longs: String,
+  enums: String,
+  arrays: true,
 });
 
 var todoProto = grpc.loadPackageDefinition(packageDefinition);
 
 const server = new grpc.Server();
 const todo = [
-	{
-		id: 1,
-		title: "Title 1",
-		body: "Body 1"
-	},
-	{
-		id: 2,
-		title: "Title 2",
-		body: "Body 2"
-	},
-	{
-		id: 3,
-		title: "Title 3",
-		body: "Body 3"
-	},
-
+  {
+    id: 1,
+    title: "Title 1",
+    body: "Body 1",
+  },
+  {
+    id: 2,
+    title: "Title 2",
+    body: "Body 2",
+  },
+  {
+    id: 3,
+    title: "Title 3",
+    body: "Body 3",
+  },
 ];
 
 server.addService(todoProto.TodoService.service, {
-	getAll: (_, callback) => {
-		callback(null, { todo });
-	},
+  getAll: (_, callback) => {
+    callback(null, { todo });
+  },
 
-	get: (call, callback) => {
-		let item = todo.find(n => n.id == call.request.id);
+  get: (call, callback) => {
+    let item = todo.find((n) => n.id == call.request.id);
 
-		if (item) {
-			callback(null, item);
-		} else {
-			callback({
-				code: grpc.status.NOT_FOUND,
-				details: "Not found"
-			});
-		}
-	},
+    if (item) {
+      callback(null, item);
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: "Not found",
+      });
+    }
+  },
 
-	insert: (call, callback) => {
-		let id = todo[todo.length -1].id +1;
-		let item = call.request;
-		
-		item.id = id;
-		todo.push(item);
-		callback(null, item);
-	},
+  insert: (call, callback) => {
+    let id = todo[todo.length - 1].id + 1;
+    let item = call.request;
 
-	update: (call, callback) => {
-		let item = todo.find(n => n.id == call.request.id);
+    item.id = id;
+    todo.push(item);
+    callback(null, item);
+  },
 
-		if (item) {
-			item.title = call.request.title;
-			item.body = call.request.body;
-			callback(null, item);
-		} else {
-			callback({
-				code: grpc.status.NOT_FOUND,
-				details: "Not found"
-			});
-		}
-	},
+  update: (call, callback) => {
+    let item = todo.find((n) => n.id == call.request.id);
 
-	remove: (call, callback) => {
-		let item = todo.findIndex(
-			n => n.id == call.request.id
-		);
+    if (item) {
+      item.title = call.request.title;
+      item.body = call.request.body;
+      callback(null, item);
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: "Not found",
+      });
+    }
+  },
 
-		if (item != -1) {
-			todo.splice(item, 1);
-			callback(null, {});
-		} else {
-			callback({
-				code: grpc.status.NOT_FOUND,
-				details: "Not found"
-			});
-		}
-	}
+  remove: (call, callback) => {
+    let item = todo.findIndex((n) => n.id == call.request.id);
+
+    if (item != -1) {
+      todo.splice(item, 1);
+      callback(null, {});
+    } else {
+      callback({
+        code: grpc.status.NOT_FOUND,
+        details: "Not found",
+      });
+    }
+  },
 });
 
 server.bind("127.0.0.1:20027", grpc.ServerCredentials.createInsecure());
